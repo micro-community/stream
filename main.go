674089568e768022950503791b2de2d28a98b8a4
Edward <chenlulei@gmail.com>
2020-04-15 10:57:56 +0800

    change to go-micro/web

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"github.com/micro/go-micro/v2"
+	"net/http"
+
 	log "github.com/micro/go-micro/v2/logger"
+	"github.com/micro/go-micro/v2/web"
 )
 
 //Meta Data
@@ -11,13 +13,19 @@ var (
 	Address = ":8080"
 )
 
+// this is still a toy code block
 func main() {
 
-	srv := micro.NewService(micro.Version("v1.0.0"))
+	//support websocket directly,by go-micro
+	srv := web.NewService(web.Version("v1.0.0"))
 
 	srv.Init()
+	// static files
+	srv.Handle("/websocket/", http.StripPrefix("/websocket/", http.FileServer(http.Dir("html"))))
 
+	//toy code ,will be changed.
 	go Run("config.toml")
+
 	// Run service
 	if err := srv.Run(); err != nil {
 		log.Fatal(err)
