8bdb1769f68fded83241e38791a66722554f645d
Edward <chenlulei@gmail.com>
2020-05-06 15:31:44 +0800

    move default to main package

--- defaults.go
+++ defaults.go
@@ -1,7 +1,6 @@
-package plugins
+package main
 
 import (
-	_ "github.com/micro-community/x-streaming/plugins/gateway"
 	_ "github.com/micro-community/x-streaming/plugins/jessica"
 	_ "github.com/micro-community/x-streaming/plugins/logrotate"
 )
